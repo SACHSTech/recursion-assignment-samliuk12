0962e4570ae4bdd61c05437662c666acfce9bd5d
samliu12 <sam.liu21@ycdsbk12.ca>
2020-12-03 15:50:50 +0000

    refractor test 4

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -4,17 +4,12 @@ public class Recursion {
 
   public static int count8(int intNumber) {
 
-    if (intNumber < 10) {
-      if (intNumber == 8) {
-        return 1;
-      }
-      else {
-        return 0;
-      }
+    if (intNumber == 0) {
+      return 0;
     }
 
     else if (intNumber % 10 == 8) {
-      return 1 + count8(intNumber / 10);
+      return 1 + count8(intNumber / 10); 
     }
 
     else {
