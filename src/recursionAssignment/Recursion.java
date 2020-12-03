27f40543753c9439d61346c8fd3174a4c9d07119
samliu12 <sam.liu21@ycdsbk12.ca>
2020-12-03 15:50:09 +0000

    passes test 4

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -4,16 +4,21 @@ public class Recursion {
 
   public static int count8(int intNumber) {
 
-    String strNumber;
+    if (intNumber < 10) {
+      if (intNumber == 8) {
+        return 1;
+      }
+      else {
+        return 0;
+      }
+    }
 
-    strNumber = Integer.toString(intNumber);
-
-    if (strNumber.equals("0")) {
-      return 0;
+    else if (intNumber % 10 == 8) {
+      return 1 + count8(intNumber / 10);
     }
 
     else {
-      return strNumber.length();
+      return count8(intNumber / 10);
     }
 
   }
